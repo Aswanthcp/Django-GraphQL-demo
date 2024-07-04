db536beefcf3b2ee63cb0740668e4ac574c8fcee
Aswanthcp <cpaswanthpalayad@gmail.com>
2024-07-04 16:26:55 +0530

    added readme

--- UserAuth_grapQL/app/schema.py
+++ UserAuth_grapQL/app/schema.py
@@ -6,10 +6,7 @@ from graphql_auth import mutations
 
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
-    token_auth = mutations.ObtainJSONWebToken.Field()
-    refresh_token = mutations.RefreshToken.Field()
-
+   
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass
