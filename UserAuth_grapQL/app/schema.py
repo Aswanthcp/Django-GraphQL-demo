d642f316be648ff6b9111b27aa5b19c17cafca9e
Aswanthcp <cpaswanthpalayad@gmail.com>
2024-07-04 16:49:18 +0530

    added latest code

--- UserAuth_grapQL/app/schema.py
+++ UserAuth_grapQL/app/schema.py
@@ -6,7 +6,10 @@ from graphql_auth import mutations
 
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
-   
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass
